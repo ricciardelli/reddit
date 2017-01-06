9cedf65dbfe72bb7dbd44f889e4e46426b728953
Richard Ricciardelli <ricciardelli2021@gmail.com>
2017-01-06 14:23:35 -0400

    :racehorse: Refactor activities

--- app/src/main/java/com/rappi/reddit/activity/MainActivity.java
+++ app/src/main/java/com/rappi/reddit/activity/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 Richard Ricciardelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rappi.reddit.activity;
 
 import android.os.Bundle;
@@ -7,7 +23,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -22,7 +37,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends ParentActivity
         implements NavigationView.OnNavigationItemSelectedListener, Callback<DefaultResponse> {
 
     @Override
@@ -113,12 +128,14 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
         if (response.isSuccessful()) {
-            Log.
+            Log.d(LOG_TAG, "Response :: " + response.body());
+        } else {
+            Log.e(LOG_TAG, "Response failed");
         }
     }
 
     @Override
     public void onFailure(Call<DefaultResponse> call, Throwable t) {
-
+        Log.e(LOG_TAG, t.getMessage(), t);
     }
 }
