813c35f1d0c8a983129ea7e7406becfea1bf448e
Richard Ricciardelli <ricciardelli2021@gmail.com>
2017-01-06 11:43:28 -0400

    :star: Add child model

---
+++ app/src/main/java/com/rappi/reddit/model/Child.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2017 Richard Ricciardelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rappi.reddit.model;
+
+/**
+ * Base class used to define children on response.
+ *
+ * @author Richard Ricciardelli
+ * @version 1.0
+ */
+public class Child {
+
+    private String kind;
+    private Reddit data;
+
+    public Child() {
+    }
+
+    public Child(String kind, Reddit data) {
+        this.kind = kind;
+        this.data = data;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public Reddit getData() {
+        return data;
+    }
+
+    public void setData(Reddit data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "kind='" + kind + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
